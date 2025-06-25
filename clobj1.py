e8b2c4c1f0035499b41c4a151560bf118b0a2851
Aswin337 <aswinkumar11a106.2122@gmail.com>
2025-06-25 19:33:09 +0530

    clobj1.py file added

---
+++ clobj1.py
@@ -0,0 +1,8 @@
+class math:
+    def square(self,num):
+        return num*num
+    def cube(self,num):
+        return num*num*num
+m=math()
+print("square :",m.square(7))
+print("cube :",m.cube(3))
